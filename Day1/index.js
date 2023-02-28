2bbc2ab8c940d32b83b83da410cd9bdedf5910bc
Hasan Ali <hasansanad73@gmail.com>
2023-02-28 14:45:20 +0700

    fundamental

---
+++ Day1/index.js
@@ -0,0 +1,17 @@
+console.log('Hello World');
+
+let message = 'hello';
+console.log(message);
+
+let number = 123;
+number = 456;
+
+const tes = [];
+console.log(typeof tes);
+
+// let coba = console;
+let coba2 = console.log();
+// console.log(typeof coba2);
+
+
+// Belajar membuat pseudocode (Kerangka dalam bahasa sebelum menulis kode bahasa pemrograman)
